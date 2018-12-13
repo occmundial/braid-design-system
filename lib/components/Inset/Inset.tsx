588c03855764c4e9fa28a86da409723f3b4a97ac
Jye Nicolson <jyenicolson@gmail.com>
2018-12-13 14:36:31 +1100

    Feat/srp hack inset (#72)
    * feat(components): Add Inset component
    
    * feat(components): Add Inset component
    
    * feat(components): Add inset component

---
+++ lib/components/Inset/Inset.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Box from '../Box/Box';
+import withTheme from '../private/withTheme';
+import { SpacingVariants } from '../../themes/theme';
+
+interface Props {
+  verticalPadding: SpacingVariants;
+}
+
+const Inset: React.StatelessComponent<Props> = ({
+  verticalPadding,
+  ...restProps
+}) => {
+  return (
+    <Box
+      paddingTop={verticalPadding}
+      paddingBottom={verticalPadding}
+      paddingLeft="gutter"
+      paddingRight="gutter"
+      {...restProps}
+    />
+  );
+};
+
+Inset.defaultProps = {
+  verticalPadding: 'medium'
+};
+
+Inset.displayName = 'Inset';
+
+export default withTheme(Inset);
